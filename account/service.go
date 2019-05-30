53c60df0cdc6325c45bd115cfe9e40a47539061e
Matheus Martins <mathrm@gmail.com>
2019-05-30 14:19:19 +0200

    add repository to account service.

--- account/service.go
+++ account/service.go
@@ -1,6 +1,14 @@
 package account
 
-type Service struct{}
+type Service struct {
+	repository Repository
+}
+
+func (service Service) WithRepository(repository Repository) error {
+	service.repository = repository
+
+	return nil
+}
 
 func (service Service) SignUp(email string, password string) {
 
