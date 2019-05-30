c3b92616aa0a35b412172008f115fe9ad65a44f7
Matheus Martins <mathrm@gmail.com>
2019-05-30 22:32:41 +0200

    account service bootstrapping for integration tests

---
+++ features/bootstrap/account.go
@@ -0,0 +1,48 @@
+package bootstrap
+
+import (
+	"github.com/DATA-DOG/godog"
+	"github.com/DATA-DOG/godog/gherkin"
+	"math3usmartins.com/todo-api-golang/account"
+	"math3usmartins.com/todo-api-golang/database/mysql"
+	"math3usmartins.com/todo-api-golang/features/bootstrap/account/repository"
+)
+
+type AccountContext struct {
+	service account.Service
+}
+
+func (accountContext AccountContext) Init(adapter mysql.Adapter) error {
+	var err error
+
+	accountRepository, err := repository.NewRepository(adapter)
+
+	if err != nil {
+		return err
+	}
+
+	service := account.Service{}
+
+	err = service.WithRepository(accountRepository)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (accountContext AccountContext) TheAccountServiceIsCalledToCreateAUserWithTheFollowingInformation(dataTable *gherkin.DataTable) error {
+	row := dataTable.Rows[1]
+
+	accountContext.service.SignUp(
+		row.Cells[0].Value,
+		row.Cells[1].Value,
+	)
+
+	return nil
+}
+
+func (accountContext AccountContext) AUserCanBeFoundByEmail(email string) error {
+	return godog.ErrPending
+}
